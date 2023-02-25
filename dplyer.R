8a26455fac270af554319d3f9baf2885edac91ce
rezadawodi1 <124818281+rezadawodi1@users.noreply.github.com>
2023-02-24 18:39:34 -0800

    Add files via upload

--- dplyer.R
+++ dplyer.R
@@ -9,12 +9,10 @@ names(data)
 dim(data)
 #Let's now filter the rows for only female students!
 fdata = filter(data, Gender == 'Female')
-fdata
 head(fdata)
 dim(fdata)
 #Let's find those females who don't smoke!
 fnsmoke = filter(data, Gender == 'Female', Smoker == "No" )
-fnsmoke
 head(fnsmoke)
 dim(fnsmoke)
 #Logical Operations ! & | and numerical data < > ==
@@ -27,7 +25,7 @@ youthemployees = filter(data, Age >= 18 & Age < 25)
 youthemployees
 #Candidates for the volleyball team
 volleyball = filter(data, Height >= 175 | Smoker == "No")
-volleyball
+head(volleyball)
 nrow(volleyball)
 #Employees who are not single
 notsingle = filter(data, Marital_Status != "Single")
@@ -43,10 +41,10 @@ agerange = filter(data, Age %in% c(18,28,36))
 agerange
 #Arrange (Organize our data in Order)
 head(arrange(data, -desc(Age)))
-arrange(data, Edu_Degree)
+head(arrange(data, Edu_Degree))
 #(Select) a specific part of the dataset
 try_select = select(data, c(Gender, Edu_Degree,Smoker))
-try_select
+head(try_select)
 #Select from a specific range 
 head(select(data, Age:Marital_Status))
 #descending order
@@ -62,3 +60,11 @@ head(newcolunm)
 summarise(data, mean(Age), sd(Weight), mean(Weight),sd(Height))
 sk = group_by(data, Smoker)
 sk
+summarise(sk, mean(Weight), sd(Weight), sd(Weight))
+#Let's Pull a column as a vector!
+vec = pull(data, Gender)
+head(vec)
+#Sample from the population :)
+smp = sample_n(data,15)
+head(smp)
+View(smp)
